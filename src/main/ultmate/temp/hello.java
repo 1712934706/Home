0968463e027c3d30cfad3a00646ba19421f69c79
masterpiece <1712934706@qq.com>
2018-12-21 00:33:03 +0800

    引入GSON

--- src/main/ultmate/temp/hello.java
+++ src/main/ultmate/temp/hello.java
@@ -1,22 +1,34 @@
 package temp;
 
+import com.google.gson.Gson;
+import model.User;
+import org.springframework.boot.json.GsonJsonParser;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class hello {
+
     public static void mainHello() {
+
+
 
         System.out.print("Hello world!");
         hello h = new hello();
         h.testThread();
 
-        Map<String,String> hashMapTemp = new HashMap<String, String>();
+        Map<String, String> hashMapTemp = new HashMap<String, String>();
 
-        hashMapTemp.put("123","456");
+        hashMapTemp.put("123", "456");
         hashMapTemp.get("123");
-
 
 
     }
@@ -26,10 +38,16 @@ public class hello {
      * 线程池以及任务，callable接口
      */
     public void testThread() {
+
+
+
+
+
+
         ThreadCall threadCall = new ThreadCall();
 
         //线程池
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 10,100,TimeUnit.MILLISECONDS,new ArrayBlockingQueue<Runnable>(5));
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 10, 100, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5));
 
         ExecutorService executorService = Executors.newCachedThreadPool();
 
@@ -56,8 +74,63 @@ public class hello {
     /**
      * runable线程测试
      */
+    public String test(String s, int numRows) {
+        if (null == s || s.length() < 1) {
+            return "";
+        }
+        if (numRows < 2) {
+            return s;
+        }
+        Character[][] str = new Character[numRows][s.length()];
+
+        int row = 0, col = 0;
 
 
+        for (int i = 0; i < s.length(); i++) {
+            if (row == numRows - 1) {
+                str[row][col] = s.charAt(i);
+                while (true) {
+                    if (--row == 0) {
+                        i++;
+                        break;
+                    } else {
+                        if (i < s.length() - 1) {
+                            str[row][++col] = s.charAt(++i);
+                        }
+                    }
+
+                }
+
+            } else {
+                str[row][col] = s.charAt(i);
+                while (true) {
+                    if (++row == numRows - 1) {
+                        i++;
+                        break;
+                    } else {
+                        if (i < s.length() - 1) {
+                            str[row][col] = s.charAt(++i);
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < s.length(); j++) {
+                if (null != str[i][j]) {
+                    sb.append(str[i][j]);
+                }
+            }
+        }
+
+        return sb.toString();
+
+    }
 
 
 }
