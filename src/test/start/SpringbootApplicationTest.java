eac42a36f9021dafe22ede7fdb86f7f46c491722
masterpiece <1712934706@qq.com>
2019-02-08 23:32:36 +0800

    修改spring boot启动扫描包，swagger扫描成功

--- src/test/start/SpringbootApplicationTest.java
+++ src/test/start/SpringbootApplicationTest.java
@@ -1,7 +1,6 @@
 package start;
 
 import com.alibaba.fastjson.JSON;
-import mapper.UserMapper;
 import model.User;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import service.UserService;
 
 
 /**
@@ -24,18 +24,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SpringbootApplicationTest {
 
     @Autowired
-    UserMapper userMapper;
+    UserService userService;
     @Autowired
     private RedisTemplate redisTemplate;
 
     @Test
     public void contextLoads() {
 
-        User user = userMapper.selectUserById(1);
+        User user = userService.getUserInfoByID(1);
 
         System.out.println(JSON.toJSONString(user));
-        redisTemplate.opsForValue().set("key2", "value2");
-        Assert.assertEquals("value2", redisTemplate.opsForValue().get("key2"));
+        redisTemplate.opsForValue().set("key4", "value4");
+        Assert.assertEquals("value4", redisTemplate.opsForValue().get("key4"));
         System.out.println(redisTemplate.opsForValue().get("key2"));
     }
 
